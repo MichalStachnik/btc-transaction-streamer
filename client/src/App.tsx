f6cbce216c88cfcfddb2c2daa498f8a3e14fbb5f
Michal Stachnik <stachnikmichal@gmail.com>
2019-11-08 12:47:07 -0500

    Attempting first deployment

---
+++ client/src/App.tsx
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
+
+import Header from './components/Header/Header';
+import BlockList from './components/BlockList/BlockList';
+import About from './components/About/About';
+import Graph from './components/Graph/Graph';
+
+import './App.css';
+
+class App extends Component<{}, { isLoading: boolean }> {
+  constructor(props: any) {
+    super(props);
+
+    this.state = {
+      isLoading: false
+    }
+  }
+
+  toggleLoading = () => {
+    this.setState({ isLoading: !this.state.isLoading });
+  }
+  
+  render() {
+    return (
+      <div className='App'>
+        <Header
+          isLoading={this.state.isLoading}
+        />
+        <Route exact path='/'>
+          <BlockList 
+            className='block-list-container'
+            toggleLoading={this.toggleLoading}
+          />
+        </Route>
+        <Route exact path='/btc/chart'>
+            <Graph/>
+        </Route>
+        <Route exact path='/about'>
+          <About/>
+        </Route>
+      </div>
+    );
+  }
+}
+
+export default App;
